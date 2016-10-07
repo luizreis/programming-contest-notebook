df62f300a303017d538040eedfcc3b07087ea284
Luiz Reis <luizreis@outlook.com>
2016-10-07 11:40:48 -0300

    Add lobos stark
    Signed-off-by: Luiz Reis <luizreis@outlook.com>

---
+++ cadernaveis/lobosstark.cpp
@@ -0,0 +1,95 @@
+/*
+function stableMatching {
+    Initialize all m ∈ M and w ∈ W to free
+    while ∃ free man m who still has a woman w to propose to {
+       w = first woman on m’s list to whom m has not yet proposed
+       if w is free
+         (m, w) become engaged
+       else some pair (m', w) already exists
+         if w prefers m to m'
+            m' becomes free
+           (m, w) become engaged 
+         else
+           (m', w) remain engaged
+    }
+}
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, cid = 1, wid = 1, cc, cw;
+list<int> pc[201];
+int pw[201][201];
+string str;
+unordered_map<string, int> c, w;
+string nc[201], nw[201];
+vector<pair<int, int> > marriage;
+
+void stable_marriage() {
+    marriage.clear();
+    int m[201];
+    list<int> fc; // free children
+    memset(m, 0, sizeof(m)); // matches (wolf -> child)
+    for (int i = 1; i <= n; i++) fc.push_back(i); // mark every child as free
+    while (fc.size() > 0) { // while there is a free child
+        int _c = fc.front(); fc.pop_front();
+        int _w = pc[_c].front(); pc[_c].pop_front(); // best match in child list
+        if (!m[_w]) {
+            m[_w] = _c; // if wolf is free match them
+        } else {
+            if (pw[_w][_c] < pw[_w][m[_w]]) { // wolf prefers current child
+                fc.push_back(m[_w]); // set other child as free
+                m[_w] = _c; // match wolf and current child
+            } else {
+                fc.push_front(_c); // c remains free
+            }
+        }
+    }
+    for (int i = 1; i <= n; i++)
+        marriage.push_back(make_pair(m[i], i));
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> str;
+        if (!c[str]) {
+            cc = c[str] = cid++;
+            nc[cc] = str;
+        }
+        else cc = c[str];
+        for (int j = 0; j < n; j++) {
+            cin >> str;
+            if (!w[str]) {
+                cw = w[str] = wid++;
+                nw[cw] = str;
+            }
+            else cw = w[str];
+            pc[cc].push_back(cw);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> str;
+        if (!w[str]) {
+            cw = w[str] = wid++;
+            nw[cw] = str;
+        }
+        else cw = w[str];
+        for (int j = 0; j < n; j++) {
+            cin >> str;
+            if (!c[str]) {
+                cc = c[str] = cid++;
+                nc[cc] = str;
+            }
+            else cc = c[str];
+            pw[cw][cc] = j;
+        }
+    }
+    stable_marriage();
+    sort(marriage.begin(), marriage.end());
+    for (int i = 0; i < marriage.size(); i++)
+        cout << nc[marriage[i].first] << ' ' << nw[marriage[i].second] << '\n';
+    return 0;
+}
